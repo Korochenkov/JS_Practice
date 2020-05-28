e2ad360acc04499a9ba3ca23b9a3f4905dc1b057
Korochenkov <Korochenkov@users.noreply.github.com>
2020-05-28 23:41:11 +0300

    Commit

--- Lesson_2/JS/main.js
+++ Lesson_2/JS/main.js
@@ -2,9 +2,9 @@
 // 'use strict';
 
 
-let oper1 = +prompt("Please input an operand #1");
-let oper2 = +prompt("Please input an operand #2");
-let answer = oper1 + oper2;
+let operand1 = +prompt("Please input an operand #1");
+let operand2 = +prompt("Please input an operand #2");
+let answer = operand1 + operand2;
 console.log(answer);
 
 
@@ -15,5 +15,5 @@ alert(`Hello, ${Name}`);
 let numForMod_2 = parseInt(prompt("Please input a number"));
 console.log(`Remainder of the division by 2: ${numForMod_2 % 2}`);
 
-let numForMod_2 = parseFloat(prompt("Please input a number"));
-console.log(`Inteder part = ${Math.trunc(numForMod_2)}, fractional part = ${numForMod_2 % 1}`);
+let floatNum = parseFloat(prompt("Please input a number"));
+console.log(`Integer part = ${Math.trunc(floatNum)}, fractional part = ${floatNum2 % 1}`);
